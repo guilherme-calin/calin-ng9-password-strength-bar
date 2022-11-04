f32d6d59b9579d1933ea071675695d8fc5ec3a36
Guilherme Calin <guilherme.s.calin@gmail.com>
2022-11-04 16:11:34 -0300

    core: module fixes

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export { Ng9PasswordStrengthBarModule } from './projects/ng9-password-strength-bar/src/lib/ng9-password-strength-bar.module';
--- projects/ng9-password-strength-bar-app/src/app/app.module.ts
+++ projects/ng9-password-strength-bar-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Ng9PasswordStrengthBarModule } from 'ng9-password-strength-bar';
+import { Ng9PasswordStrengthBarModule } from '../../../ng9-password-strength-bar/src/lib/ng9-password-strength-bar.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
