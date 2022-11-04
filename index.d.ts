b87a3121b7edd3a1a072d30b6ba653f5a8a49753
Guilherme Calin <guilherme.s.calin@gmail.com>
2022-11-04 16:30:56 -0300

    chore: index.d file change

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,1 @@
-export * from './projects/ng9-password-strength-bar/src/public-api';
+export { Ng9PasswordStrengthBarModule, Ng9PasswordStrengthBarComponent } from './projects/ng9-password-strength-bar/src/public-api';
